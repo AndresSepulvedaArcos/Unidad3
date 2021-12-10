a8559782dae2c9040aa2838e6d0cec2fee6759a7
Andres Sepulveda <63136946+AndresSepulvedaArcos@users.noreply.github.com>
2021-12-10 17:49:43 -0300

    vertex property block

---
+++ Assets/Scripts/ParticleSystem/ParticleSystemComtroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemComtroller : MonoBehaviour
+{
+    public GameObject ps;
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        { 
+            ps.SetActive(true);
+            particleSystem.Play();
+            }
+    }
+}
---
+++ Assets/Scripts/VertexController/VertexController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexController : MonoBehaviour
+{
+    public float mySpeed=0;
+    MaterialPropertyBlock propertyBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+          propertyBlock = new MaterialPropertyBlock();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propertyBlock.SetFloat("Speed", mySpeed);
+        //   GetComponent<MeshRenderer>().sharedMaterial.SetFloat("Speed",mySpeed);
+        GetComponent<MeshRenderer>().SetPropertyBlock(propertyBlock);
+    }
+}
